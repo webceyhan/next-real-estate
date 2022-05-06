84d10fd4d4f22a36d451a9f99194da8df55491a1
Evren Ceyhan <evren@ceyhan.io>
2022-05-06 16:24:21 +0300

    search no-result fallback impl.

--- pages/search.jsx
+++ pages/search.jsx
@@ -4,6 +4,9 @@ import { BsFilter } from 'react-icons/bs';
 import { Box, Flex, Icon, Text } from '@chakra-ui/react';
 
 import { SearchFilters } from '../components/SearchFilters';
+import { Property } from '../components/Property';
+import Image from 'next/image';
+import NoResultImg from '../assets/no-result.svg';
 
 export default function Search() {
     const router = useRouter();
@@ -26,7 +29,33 @@ export default function Search() {
                 <Text>Search Propery By Filters</Text>
                 <Icon paddingLeft="2" w="7" as={BsFilter} />
             </Flex>
+
             {searchFilters && <SearchFilters />}
+
+            <Text fontSize="2xl" p="4" fontWeight="bold">
+                Properties {router.query.purpose}
+            </Text>
+
+            <Flex flexWrap="wrap">
+                {[].map((property) => (
+                    <Property key={property.id} property={property} />
+                ))}
+            </Flex>
+
+            {[].length === 0 && (
+                <Flex
+                    justifyContent="center"
+                    alignItems="center"
+                    flexDir="column"
+                    marginTop="5"
+                    marginBottom="5"
+                >
+                    <Image alt="no result" src={NoResultImg} />
+                    <Text fontSize="2xl" marginTop="3">
+                        No Result Found
+                    </Text>
+                </Flex>
+            )}
         </Box>
     );
 }
